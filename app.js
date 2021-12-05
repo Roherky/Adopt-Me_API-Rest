68e538d2d15f99b6d48f70e7312f3a1f1a292d5b
Alex Carrasco Balart <balartalex3@gmail.com>
2021-12-05 18:04:21 +0100

    Primer push

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mysqlConnection  = require('./config/sql');
+const cors = require ('cors');
+// Routes
+const adoptante = require('./app/routers/adoptante');
+const animal = require('./app/routers/animal');
+const login_registro = require('./app/routers/login_registro');
+
+
+app.use(cors());
+app.use(express.json());
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(express.json());
+app.use(animal);
+app.use(adoptante);
+app.use(login_registro);
+
+
+
+
+
+// Iniciar Servidor
+app.listen(app.get('port'), () => {
+  console.log(`Servidor en Puerto ${app.get('port')}`);
+});
