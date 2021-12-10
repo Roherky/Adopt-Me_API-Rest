6ce1fdb7f43964f927c705079c691446217a3da5
Roherky <rodrigo31996@gmail.com>
2021-12-10 20:59:25 +0100

    Prueba Heroku

--- config/sql.js
+++ config/sql.js
@@ -2,11 +2,7 @@ const mysql = require('mysql2');
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-<<<<<<< HEAD
     password: 'B4l4rt4l3x#12',
-=======
-    password: 'CodenotchMySQL',
->>>>>>> prueba1
     database: 'adot-me',
     // multipleStatements: true
 });
