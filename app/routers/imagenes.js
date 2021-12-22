22d0f3d49e93b4af59a5fa0acca956cd0665783c
esthermoralesM <esthermoraless675@gmail.com>
2021-12-22 14:27:34 +0100

    imagenes

--- app/routers/imagenes.js
+++ app/routers/imagenes.js
@@ -8,6 +8,7 @@ routes.get('/imagenes',(req, res)=>{
     let sql;
     var id_animal = req.query.id_animal;
     var id_protectora = req.query.id_protectora;
+    console.log('id_protectora');
 
     if(id_protectora!=null){
         sql="SELECT * FROM imagenes WHERE id_protectora="+id_protectora +" AND id_animal=0";
