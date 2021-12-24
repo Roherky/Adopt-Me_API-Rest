58bcc0da4664614c0003864ba596ac87e05eab48
esthermoralesM <esthermoraless675@gmail.com>
2021-12-24 12:46:35 +0100

    protectora animales

--- app/routers/animal.js
+++ app/routers/animal.js
@@ -54,7 +54,11 @@ routes.get('/animal',(req, res)=>{
         }
     }
     else{
+        if(idProtec==false){
         sql = `${sql} WHERE estado='adopcion'`
+        }else{
+            sql=sql;
+        }
     }
 
     mysqlConnection.query(sql, function (err, result) {
